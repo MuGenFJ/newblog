9f216d584edd87372393f35c32f1af48d6ebba29
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2020-11-02 13:25:57 +0100

    disqus plugin & share links added

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -7,9 +7,25 @@ import Img from 'gatsby-image'
 import { slugify } from '../util/utilityFunctions'
 import authors from '../util/authors'
 
-const singlePost = ({ data }) => {
+import { FiFacebook } from 'react-icons/fi';
+import { SiTwitter } from 'react-icons/si';
+import { AiFillLinkedin } from 'react-icons/ai';
+
+import { DiscussionEmbed } from 'disqus-react';
+
+const singlePost = ({ data, pageContext }) => {
     const post = data.markdownRemark.frontmatter
     const author = authors.find(x => x.name === post.author)
+
+    const baseUrl = "https://myblog.co.uk/"
+
+    const disqusShortname = "https-myblog-co-uk"
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug
+    }
+
     return (
         <Layout pageTitle={post.title} postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}>
             <SEO title={post.title} />
@@ -32,6 +48,29 @@ const singlePost = ({ data }) => {
                     </ul>
                 </CardBody>
             </Card>
+            <h3 className="text-center">
+                Share this post
+            </h3>
+            <div className="text-center social-share-links">
+                <ul>
+                    <li>
+                        <a href={'https://www.facebook.com/sharer/sharer.php?u=' + baseUrl + pageContext.slug} className="facebook" target="_blank" rel="noopener noreferrer">
+                            <FiFacebook />
+                        </a>
+                    </li>
+                    <li>
+                        <a href={'https://twitter.com/'} className="twitter" target="_blank" rel="noopener noreferrer">
+                            <SiTwitter />
+                        </a>
+                    </li>
+                    <li>
+                        <a href={'https://www.linkedin.com/shareArticle?url=' + baseUrl + pageContext.slug} className="linkedin" target="_blank" rel="noopener noreferrer">
+                            <AiFillLinkedin />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
         </Layout>
     )
 }
