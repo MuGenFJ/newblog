6f48443afc49fedc49a1a29b6ad6b2e7b4ffec78
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2020-11-02 17:19:41 +0100

    tags page & counts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const { slugify } = require(`./src/util/utilityFunctions`);
 const path = require('path') //we will be able to search the path in our project
 const authors = require('./src/util/authors')
+const _ = require('lodash')
 
 exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
@@ -16,7 +17,11 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions;
-    const singlePostTemplate = path.resolve("src/templates/single-post.js")
+
+    const templates = {
+        singlePost: path.resolve("src/templates/single-post.js"),
+        tagsPage: path.resolve("src/templates/tags-page.js"),
+    }
 
     return graphql(`
         {
@@ -39,10 +44,11 @@ exports.createPages = ({ actions, graphql }) => {
 
         const posts = res.data.allMarkdownRemark.edges
 
+        // Create single blog post pages
         posts.forEach(({ node }) => {
             createPage({
                 path: node.fields.slug,
-                component: singlePostTemplate,
+                component: templates.singlePost,
                 context: {
                     //Passing slug for template to use to get post
                     slug: node.fields.slug,
@@ -50,6 +56,32 @@ exports.createPages = ({ actions, graphql }) => {
                     imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
                 }
             })
+        })
+
+        // It will assemble all the tags to the tag array
+        let tags = []
+        _.each(posts, edge => {//for each posts in the edge 
+            if (_.get(edge, 'node.frontmatter.tags')) { //get from edge the tags in frontmatter if there is something otherwise do nothing
+                tags = tags.concat(edge.node.frontmatter.tags)
+            }
+        })
+
+        // It will add the counts behind the tag
+        let tagPostCounts = {}
+        tags.forEach(tag => {
+            tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1; //[tag] is a proprety same name of the tag. if we don't have tagPostCounts[tag] it will be 0 otherwise we add 1
+        })
+
+        tags = _.uniq(tags) //eleminate all duplicate tags
+
+        // Create tags page
+        createPage({
+            path: `/tags`,
+            component: templates.tagsPage,
+            context: {
+                tags,
+                tagPostCounts,
+            }
         })
     })
 }
